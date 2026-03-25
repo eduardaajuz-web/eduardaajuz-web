6a1ec396e2a4f2e1f6fefb7aed6c2d6feb4d5f2c
duda 🕸️˙⟡ <eduarda.ajuz@escola.pr.gov.br>
2026-03-25 09:46:36 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,11 @@
+const produtos = ["pão caseiro", "sonho", "pão de queijo", "bolo", "croissant", "coxinha", "queijo", "presunto", "empada", "café" ]
+const precos = [    10.00 ,       3.00 ,      1.50 ,        6.00 ,   4.00 ,       2.50 ,     6.50 ,    4.00 ,      5.50  ,  7.00 ]
+
+
+let contador = 0 
+while(contador <= 10){ 
+    console.log(produtos[contador] + " - " + precos[contador])
+    contador = contador + 1 
+}
+
+function novoProduto
